bcc5633570d54c604c57c98b50240c712a7ba9c7
Dani <dani.gregori@imaginagroup.com>
2023-12-13 16:44:44 +0100

    first commit

---
+++ app/service/fetchData.ts
@@ -0,0 +1,60 @@
+import { request, gql } from 'graphql-request'
+
+export const getData = async () => {
+
+  const query = gql`
+    query Mains {
+      mains {
+        title
+        description
+        id
+        slug
+        body {
+          raw
+        }
+        foto {
+          url(
+            transformation: {
+              image: { resize: { width: 250, height: 250, fit: crop  } }
+            }
+          )
+        }
+        images {
+          id
+          url(
+            transformation: {
+              image: { resize: { width: 250, height: 250, fit: crop  } }
+            }
+          )
+        }
+        compMap {
+          title
+          fotoCom {
+            url(
+              transformation: {
+                image: { resize: { width: 250, height: 250, fit: crop  } }
+              }
+            )
+          }
+          id
+        }
+        
+      }
+      asides {
+        title
+        id
+        description
+        booleanAside
+        body {
+          raw
+        }
+      }
+    }
+    
+        `
+
+  const result = await request('https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clq27w8db4wab01tb5sdfcvi2/master', query)
+  return result
+}
+
+
